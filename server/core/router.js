7fa04d80f9df91e8a7e7340d603761684e4f3f2b
GuiMac <guibasconti@gmail.com>
2017-10-22 13:36:51 -0200

    Add post to send report

--- server/core/router.js
+++ server/core/router.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.use(bodyParser.json());
 
+const sendReport = require('../controllers/sendReport');
+
 //  Placeholder API
 router.get('/', (req, res) => {
   res.status(200).json({ msg: 'Hi!' });
 });
+
+router.post('/report/:telegram_id', sendReport);
 
 module.exports = router;
--- server/utils/constants.js
+++ server/utils/constants.js
@@ -6,7 +6,11 @@ module.exports = {
   messages: {
     error: {
       INVALID_TELEGRAM_ID: 'The telegram id sent is invalid.',
+      INVALID_DATA: 'No data message was sent.',
       INVALID_ERROR_MESSAGE: 'The error message sent is invalid.'
+    },
+    info: {
+      REPORT_SENT: 'The report was successful sent.'
     }
   },
   regex:{
