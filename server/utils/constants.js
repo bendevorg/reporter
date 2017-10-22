95f0c3a7cc0e6e3e907ae8306b0030116b0dc4d2
GuiMac <guibasconti@gmail.com>
2017-10-22 13:09:48 -0200

    Add constants and validator

---
+++ server/utils/constants.js
@@ -0,0 +1,15 @@
+/**
+ * All project constants
+ * @module utils/constants
+*/
+module.exports = {
+  messages: {
+    error: {
+      INVALID_TELEGRAM_ID: 'The telegram id sent is invalid.',
+      INVALID_ERROR_MESSAGE: 'The error message sent is invalid.'
+    }
+  },
+  regex:{
+    integer: /^-?\d+$/
+  }
+};
---
+++ server/utils/validator.js
@@ -0,0 +1,28 @@
+/**
+ * Module of validation functions
+ * @module controllers/validator
+*/
+
+const constants = require('./constants');
+
+/**
+ * Validate if the input is a valid not empty string
+ *
+ * @param {string} stringToValidate - String to be validated
+ * @return {boolean} - True case the string is valid and false if it is not
+*/
+exports.isValidString = stringToValidate => {
+  return String.isString(stringToValidate) && stringToValidate.trim().length > 0;
+};
+
+/**
+ * Validate if the input is a valid positive or negative integer
+ *
+ * @param {string} integerToValidate - Integer to be validated
+ * @return {boolean} - True case the integer is valid and false if it is not
+*/
+exports.isValidInteger = integerToValidate => {
+  return constants.regex.integer.test(integerToValidate) 
+    && parseInt(integerToValidate) <= Number.MAX_SAFE_INTEGER 
+    && parseInt(integerToValidate) >= Number.MIN_SAFE_INTEGER;
+};
